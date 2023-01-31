3abeae387719a896dbfdcf37de0f02b04ab33d9e
Minhyeok Son <mh.blosson@gmail.com>
2023-01-31 14:24:58 +0900

    websocket complete

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,46 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✔");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from the server 🛠");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!!!!!");
-}, 10000);
+// setTimeout(() => {
+//   socket.send("hello from the browser!!!!!");
+// }, 10000);
+
+function handleNickSumbit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+
+  input.value = "";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  // console.log(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+nickForm.addEventListener("submit", handleNickSumbit);
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -19,13 +19,25 @@ const wss = new WebSocket.Server({ server });
 // ws만 돌리길 원할 때
 // const wss = new WebSocket.Server({ server });
 
+// 서버에 누가 연결되어있는지 확인해주기 위해 [] 만들어줌
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous"; // 이런식으로 소켓 안에 정보를 저장해줄 수 있음!
   console.log("Connected to Browser ✔");
   socket.on("close", () => console.log("Disconnected from the browser!"));
   socket.on("message", (message) => {
-    console.log(message);
+    const parsed = JSON.parse(message);
+    console.log(parsed, message.toString("utf8")); // 왼쪽은 JS Object, 오른쪽은 그냥 string(parse안된 거)
+    if (parsed.type === "new_message") {
+      sockets.forEach((aSocket) => {
+        aSocket.send(`${socket.nickname}: ${parsed.payload}`);
+      });
+    } else if (parsed.type === "nickname") {
+      socket["nickname"] = parsed.payload; // 이런식으로 객체에 key-value 추가해줄 수 있음
+    }
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
